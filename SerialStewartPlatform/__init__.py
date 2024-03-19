f2726cbe44b4059391b4ebc201b544d2035f38ea
jfrabosi <jfrabosi@calpoly.edu>
2024-03-19 11:19:50 -0700

    presentation

--- SerialStewartPlatform/__init__.py
+++ SerialStewartPlatform/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
     id='SerialStewartPlatform/SerialStewartPlatform-v0',
     entry_point='envs:SerialStewartPlatform',
-    max_episode_steps=5000
+    max_episode_steps=750
 )
--- main.py
+++ main.py
@@ -17,10 +17,10 @@ num_ssp = 3
 env = gym.make("SerialStewartPlatform/SerialStewartPlatform-v0", num_ssp=num_ssp)#, render_mode="human")
 model = PPO("MlpPolicy", env, verbose=0, device="cpu")
 
-replicates = 3
-total_timesteps = [None] * replicates * 9
-mean_reward = [None] * replicates * 9
-std_reward = [None] * replicates * 9
+replicates = 1
+total_timesteps = [None] * replicates# * 9
+mean_reward = [None] * replicates# * 9
+std_reward = [None] * replicates# * 9
 index = 0
 taguchi = np.array([[0, 0, 0],
                     [1, 1, 1],
@@ -32,40 +32,41 @@ taguchi = np.array([[0, 0, 0],
                     [1, 0, 2],
                     [2, 1, 0]])
 
-for i in range(9):
-    for n in range(replicates):
-        del model
-        t1 = time.time()
-        print(index)
-        pi_layers = [num_ssp * 18 * 2 * (1+taguchi[i, 2]) for _ in range(2 + taguchi[i, 1] * 2)]
-        print(pi_layers)
-        policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=dict(pi=pi_layers, vf=pi_layers))
-        model = PPO("MlpPolicy", env, verbose=0, policy_kwargs=policy_kwargs, device="cpu")
-        total_timesteps[index] = 100000 * 2**(taguchi[i, 0])
-        print(total_timesteps[index])
-        model.learn(total_timesteps=total_timesteps[index], progress_bar=True)
-        mean_reward[index], std_reward[index] = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-        print(f"{total_timesteps[index]:<16.0f}{mean_reward[index]:<16.2f}{std_reward[index]:<16.2f}")
-        index += 1
-# model.save("ppo_test_3")
-
-# Print the table header
-print(f"{'Total Timesteps':<16}{'Mean Reward':<16}{'STD Reward':<16}")
-
-# Print the table rows
-for timesteps, mean, std in zip(total_timesteps, mean_reward, std_reward):
-    print(f"{timesteps:<16.0f}{mean:<16.2f}{std:<16.2f}")
-
-# env = gym.make("SerialStewartPlatform/SerialStewartPlatform-v0", num_ssp=num_ssp, render_mode="human")
+# # # for i in range(9):
+# for n in range(replicates):
+#     del model
+#     t1 = time.time()
+#     print(index)
+#     # pi_layers = [num_ssp * 18 * 2 * (1+taguchi[i, 2]) for _ in range(2 + taguchi[i, 1] * 2)]
+#     pi_layers = [num_ssp * 18 * 2 * 2 for _ in range(6)]
+#     # print(pi_layers)
+#     policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=dict(pi=pi_layers, vf=pi_layers))
+#     model = PPO("MlpPolicy", env, verbose=0, policy_kwargs=policy_kwargs, device="cpu")
+#     total_timesteps[index] = 2000000 #* 2**(taguchi[i, 0])
+#     print(total_timesteps[index])
+#     model.learn(total_timesteps=total_timesteps[index], progress_bar=True)
+#     mean_reward[index], std_reward[index] = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+#     print(f"{total_timesteps[index]:<16.0f}{mean_reward[index]:<16.2f}{std_reward[index]:<16.2f}")
+#     index += 1
+# model.save("ppo_test_2")
 #
-# # the policy_kwargs are automatically loaded
+# # Print the table header
+# print(f"{'Total Timesteps':<16}{'Mean Reward':<16}{'STD Reward':<16}")
 #
-# model = PPO.load("ppo_test_3", env=env)
-#
-# # Enjoy trained agent
-# vec_env = model.get_env()
-# obs = vec_env.reset()
-# for i in range(1000):
-#     action, _states = model.predict(obs)
-#     obs, rewards, dones, info = vec_env.step(action)
-#     vec_env.render()
+# # Print the table rows
+# for timesteps, mean, std in zip(total_timesteps, mean_reward, std_reward):
+#     print(f"{timesteps:<16.0f}{mean:<16.2f}{std:<16.2f}")
+
+env = gym.make("SerialStewartPlatform/SerialStewartPlatform-v0", num_ssp=num_ssp, render_mode="human")
+
+# the policy_kwargs are automatically loaded
+
+model = PPO.load("ppo_test_v4", env=env)
+
+# Enjoy trained agent
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render()
